655253639e0eb21ed3a170107027f0ffa19e8674
Sebastien Riou <sebastien.riou@pqshield.com>
2025-09-02 14:27:04 +0200

    add test_callbacks

--- test/source/main.c
+++ test/source/main.c
@@ -1,8 +1,10 @@
 #include "lean-ftl-test.h"
 void init(int argc, const char*argv[]);
 int test_main();
+void test_callbacks();
 int main(int argc, const char*argv[]){
   init(argc,argv);
+  test_callbacks();
   return test_main();
 }
 

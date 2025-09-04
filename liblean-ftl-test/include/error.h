07f076edd3ee196d2971ca520db3b932c1f805d8
Sebastien Riou <sebastien.riou@pqshield.com>
2025-09-04 10:07:07 +0200

    avoid collision in error codes

--- liblean-ftl-test/include/error.h
+++ liblean-ftl-test/include/error.h
@@ -3,10 +3,10 @@
 #define CONST_OK 0
 #define CONST_NOT_SET 0xFFFFFFFF
 
-#define INTERNAL_ERROR_CORRUPT              0x00000005
-#define ERROR_BUTTON_PRESSED                0x00000008
-#define INTERNAL_ERROR_NVM_WRITE            0x00000009
-#define ERROR_VERIFICATION_FAIL             0x0000000A
-#define ERROR_NOT_IMPLEMENTED               0x0000000B
+#define INTERNAL_ERROR_CORRUPT              0xFF000005
+#define ERROR_BUTTON_PRESSED                0xFF000008
+#define INTERNAL_ERROR_NVM_WRITE            0xFF000009
+#define ERROR_VERIFICATION_FAIL             0xFF00000A
+#define ERROR_NOT_IMPLEMENTED               0xFF00000B
 
 void throw_exception(uint32_t err_code);

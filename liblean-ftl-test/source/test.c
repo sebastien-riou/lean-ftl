0d3f80877ab5d70e2e27e75f8c0b1293a6f529a7
Sebastien Riou <sebastien.riou@pqshield.com>
2026-08-20 22:22:30 +0200

    fix bug in test

--- liblean-ftl-test/source/test.c
+++ liblean-ftl-test/source/test.c
@@ -438,7 +438,7 @@ void write_nvm_to_nvm_test_core(unsigned int dst_offset, void*test_storage, unsi
   //set src in area b with same alignement as test_storage
   const bool test_storage_aligned = (uintptr_t)test_storage%2 ? 0 : 1;
   const bool src_aligned = (uintptr_t)src%2 ? 0 : 1;
-  if(test_storage_aligned & !src_aligned){
+  if(test_storage_aligned){//} & !src_aligned){
     src = ((uint8_t*)base_b);
     test_write2(&nvmb,src,&nvm.unmanaged_data0,test_storage_size,test_storage);
     SANITY_CHECK();
